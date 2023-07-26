c98b204427527d24c60a40009e8e401a600c8a44
alamgirmbstu <hmdalamgir@gmail.com>
2023-07-27 02:36:17 +0600

    Add Menu.jsx - for NavBar using DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from "react";
+import Menu from "./components/Menu";
 
 const App = () => {
   return (
-    <div>
+    <div>      
+      <Menu/>
       <h1>This is APP page</h1>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
     </div>
   );
 };
