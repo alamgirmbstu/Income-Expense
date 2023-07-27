45f4c0b0f54d96f4eb4219dc1b6655ae6c39c844
alamgirmbstu <hmdalamgir@gmail.com>
2023-07-27 12:00:48 +0600

    Add React-Router-Dom NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NotFoundPage from "./Pages/NotFoundPage";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import SamplePage from './Pages/SamplePage';
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage/>} />
+          <Route path="/SamplePage" element={<SamplePage/>} />
+          <Route path="*" element={<NotFoundPage/>} />
         </Routes>
       </HashRouter> 
     </div>
---
+++ src/Pages/SamplePage.jsx
@@ -0,0 +1,12 @@
+import Menu from "../components/Menu";
+
+const SamplePage = () => {
+    return (
+        <div>
+            <Menu/>
+            <h1>This is sample Page</h1>
+        </div>
+    );
+};
+
+export default SamplePage;
