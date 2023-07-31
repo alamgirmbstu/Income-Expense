00bd845427bf1a0115f770390ee5eeb16a31a8fa
alamgirmbstu <hmdalamgir@gmail.com>
2023-07-31 18:06:08 +0600

    Update layout.jsx

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -14,7 +14,7 @@ const Layout = (props) => {
       <div className="min-h-[10vh]">
         <Menu />
       </div>
-      <div className="min-h-[60vh] flex">{props.children}</div>
+      <div className="min-h-[60vh] content-center flex">{props.children}</div>
       <div className="min-h-[25vh]">
         <Footer />
       </div>
